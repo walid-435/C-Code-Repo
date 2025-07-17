ea7fae418a41273a10a13aac8853247720852372
Walid Rahman <rahman241-35-435@diu.edu.bd>
2025-07-18 00:09:03 +0600

    commiting c files

--- CodeofSorting/bubblesort.c
+++ CodeofSorting/bubblesort.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 int main(){
     int n, temp;
@@ -29,36 +28,4 @@ int main(){
 
     printf("\n");
 
-=======
-#include <stdio.h>
-int main(){
-    int n, temp;
-
-    printf("enter the size for the array: ");
-    scanf("%d", &n);
-
-    int arr[n];
-    printf("enter the elements for the array: ");
-    for(int i=0; i<n; i++){
-        scanf("%d", &arr[i]);
-    }
-
-    for(int i=0; i<n-1; i++){
-        for(int j=0; j<n-i-1; j++){
-            if(arr[j]>arr[j+1]){
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-            }
-        }
-    }
-
-    printf("sorted array: ");
-    for(int i=0; i<n; i++){
-        printf("%d ", arr[i]);
-    }
-
-    printf("\n");
-
->>>>>>> 87d33e437f049844b6e8cb278ca3f80645c6c8f7
 }
