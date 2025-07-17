c354b554c36e97156537a16c4cd6e1732fe2efd6
Walid Rahman <rahman241-35-435@diu.edu.bd>
2025-07-18 00:12:39 +0600

    commiting c files

--- CodeofSorting/selectionsort.c
+++ CodeofSorting/selectionsort.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int swap(int *a, int *b){
@@ -39,46 +38,4 @@ int main(){
 
     printf("\n");
 
-=======
-#include <stdio.h>
-
-int swap(int *a, int *b){
-    int temp=*a;
-    *a=*b;
-    *b=temp;
-}
-
-int main(){
-    int n, min;
-
-    printf("enter the size for the array: ");
-    scanf("%d", &n);
-
-    int arr[n];
-    printf("enter the elements for the array: ");
-    for(int i=0; i<n; i++){
-        scanf("%d", &arr[i]);
-    }
-
-    for(int i=0; i<n-1; i++){
-        min=i;
-        for(int j=i+1; j<n; j++){
-            if(arr[j]<arr[min]){
-                min=j;
-            }
-        }
-
-        if(min!=i){
-            swap(&arr[i], &arr[min]);
-        }
-    }
-
-    printf("sorted array: ");
-    for(int i=0; i<n; i++){
-        printf("%d ", arr[i]);
-    }
-
-    printf("\n");
-
->>>>>>> 87d33e437f049844b6e8cb278ca3f80645c6c8f7
 }
